aa0192a3c2e8caff1dde20622e72b0c04a99f3d4
petar <petar@aktek.io>
2020-04-05 21:10:28 +0200

    create custom bandwidth tooltip

--- src/components/Main/Bandwidth/index.js
+++ src/components/Main/Bandwidth/index.js
@@ -18,6 +18,26 @@ const Container = styled.div({
   justifyContent: "center",
 });
 
+const TooltipContainer = styled.div({
+  backgroundColor: "#ffffff",
+  padding: "10px",
+  fontSize: "12px",
+});
+
+const TooltipLabelContainer = styled.div({
+  paddingBottom: "10px",
+  fontWeight: "bold",
+});
+
+const TooltipItem = styled.span`
+  color: ${(props) => props.color};
+  padding-right: ${(props) => (props.paddingRight ? props.paddingRight : null)};
+`;
+
+const TooltipContentContainer = styled.div({
+  paddingTop: "30px",
+});
+
 const getGb = (bits) => {
   const kilobits = bits / 1024;
   const megabits = kilobits / 1024;
@@ -107,6 +127,14 @@ const getMaximumCDN = (formattedData) => {
   }, 0);
 };
 
+const getTotalAndSpikeReduction = (payload) => {
+  const http = parseInt(payload[0].value);
+  const ptp = parseInt(payload[1].value);
+  const total = http + ptp;
+  const spikeReduction = Math.ceil((ptp / total) * 100);
+  return { total, spikeReduction };
+};
+
 function MemoizedBandwidth({
   data,
   selectedStartTimestampKey,
@@ -121,6 +149,51 @@ function MemoizedBandwidth({
   const maximumThroughput = getMaximumThroughput(formattedData);
 
   const maximumCDN = getMaximumCDN(formattedData);
+
+  const getTooltipContent = (tooltipProps) => {
+    const { label, payload } = tooltipProps;
+    const totalAndSpikeReduction =
+      payload.length > 0 ? getTotalAndSpikeReduction(payload) : null;
+    return (
+      <TooltipContainer>
+        <TooltipLabelContainer>
+          {moment.unix(label / 1000).format("ddd, MMMM DD, YYYY h:mm A")}
+        </TooltipLabelContainer>
+        {payload.map(({ name, value, stroke }) => (
+          <div key={name}>
+            <TooltipItem paddingRight={"10px"} color={stroke}>
+              •
+            </TooltipItem>
+            <TooltipItem
+              paddingRight={"10px"}
+              color={"#333"}
+            >{`${name}:`}</TooltipItem>
+            <TooltipItem color={stroke}>{`${value}Gbps`}</TooltipItem>
+          </div>
+        ))}
+        {totalAndSpikeReduction && (
+          <TooltipContentContainer>
+            <div>
+              <TooltipItem paddingRight={"10px"} color={"#333"}>
+                {"Total:"}
+              </TooltipItem>
+              <TooltipItem color={"#3FCB7E"}>
+                {`${totalAndSpikeReduction.total}Gbps`}
+              </TooltipItem>
+            </div>
+            <div>
+              <TooltipItem paddingRight={"10px"} color={"#333"}>
+                {"Spike reduction:"}
+              </TooltipItem>
+              <TooltipItem color={"#12A5ED"}>
+                {`${totalAndSpikeReduction.spikeReduction}%`}
+              </TooltipItem>
+            </div>
+          </TooltipContentContainer>
+        )}
+      </TooltipContainer>
+    );
+  };
 
   return (
     <Container>
@@ -139,7 +212,7 @@ function MemoizedBandwidth({
             tickFormatter={(tick) => moment.unix(tick / 1000).format("DD. MMM")}
           />
           <YAxis />
-          <Tooltip />
+          <Tooltip content={getTooltipContent} />
           <Area
             type="monotone"
             dataKey="http"
